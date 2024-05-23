7fa4dca2aed636a61ff8919a9d2db3c119292bf4
GooberShadow <benjamin.kehe@us.af.mil>
2024-05-22 23:12:55 -0500

    first commit. tutorial done
    stable. coyote jump is too high

---
+++ scenes/entities/player/FALL.gd
@@ -0,0 +1,31 @@
+extends "state.gd"
+
+@onready var CoyoteTimer = $CoyoteTime
+@export var coyote_duration = .5
+
+var can_jump = true
+func update(delta):
+	Player.gravity(delta)
+	player_movement()
+	if Player.is_on_floor():
+		return STATES.IDLE
+	if Player.dash_input and Player.can_dash:
+		return STATES.DASH
+	if Player.get_next_to_wall() != null:
+		return STATES.SLIDE
+	if Player.jump_input_actuation and can_jump:
+		return STATES.JUMP
+	return null
+
+func enter_state():
+	if Player.prev_state == STATES.IDLE or Player.prev_state == STATES.MOVE or Player.prev_state == STATES.SLIDE:
+		can_jump = true
+		CoyoteTimer.start(coyote_duration)
+	else:
+		can_jump = false
+	pass
+
+
+func _on_coyote_time_timeout():
+	can_jump = false
+	pass # Replace with function body.
